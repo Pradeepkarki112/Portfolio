182ec5d760c0a83f19c0838fca09ad333a668d6b
Pradeepkarki112 <pradeepkarki112@gmail.com>
2023-09-05 13:41:28 +0545

    corrected

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,7 +6,7 @@ const Info = () => {
       <div className="about__box about__icon">
         <i class="bx bx-award"></i>
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">Intership</span>
+        <span className="about__subtitle">Internship</span>
       </div>
       <div className="about__box">
         <i class="bx bx-briefcase-alt about__icon"></i>
